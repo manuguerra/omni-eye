90ca07123081506dc169eefa36505f304f5bc6c3
manuguerra <guerra.manuel@gmail.com>
2013-02-14 16:36:42 -0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniEye::Application.config.secret_token = 'b5208bfd0b6eab0c99dffa4241bb53b1442aa0e5d749896e3656c5214e405d4d1c63c9e9019922025e7a3985058638a2aeae0d0df3c2a1224fa400be990c540a'
